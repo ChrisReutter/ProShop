db0a3994ce1c7a5ab92b52b38adfea4c17380d0f
ChrisReutter <estaverano3@gmail.com>
2021-01-05 00:49:29 +0100

    Admin Functionality for products editing added! Fixed a bug where data was not saved after editing in ProductEditScreen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,6 +4,9 @@ import {composeWithDevTools} from "redux-devtools-extension";
 import {
   productListReducer,
   productDetailsReducer,
+  productDeleteReducer,
+  productCreateReducer,
+  productUpdateReducer,
 } from "./reducers/productReducers";
 import {cartReducer} from "./reducers/cartReducers";
 import {
@@ -21,9 +24,13 @@ import {
   orderPayReducer,
   orderListMyReducer,
 } from "./reducers/orderReducers";
+
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  productDelete: productDeleteReducer,
+  productCreate: productCreateReducer,
+  productUpdate: productUpdateReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
